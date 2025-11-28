55fab469e2d50b9a13a2d9cc89d77ec14afb794b
José Rojas <angel2296rojas@gmail.com>
2025-11-28 00:13:07 -0400

    hola

--- products-service/routes/api.php
+++ products-service/routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\orderItemController;
 use App\Http\Controllers\productController;
 use App\Http\Controllers\categoryController;
 
-Route::middleware('jwt')->group(function () {
+Route::middleware( 'jwt')->group(function () {
     Route::apiResource('orders', orderController::class);
     Route::apiResource('order-items', orderItemController::class);
     Route::apiResource('products', productController::class);
